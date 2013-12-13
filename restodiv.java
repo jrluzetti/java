a1ed80699b76a72a1c1ed160b8f69d2a2e206c69
Sergio Roberto Luzetti Junior <jrluzetti@gmail.com>
2013-12-13 15:23:28 -0200

    Create restodiv.java

---
+++ restodiv.java
@@ -0,0 +1,9 @@
+class restodiv{
+    public int resto(int divd, int divs){
+        int dividendo = divd;
+        int divisor = divs;
+        int resdiv=dividendo/divisor;
+        int resto1=dividendo-(resdiv*divisor);
+        return resto1;
+    }
+}
